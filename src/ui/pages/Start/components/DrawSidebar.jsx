fce291e90760528bad688c47f81ac79f1254a405
aldizeci <zaldin0201@gmail.com>
2026-02-02 00:08:18 +0100

    fix: fix size of checkbox

--- src/ui/pages/Start/components/DrawSidebar.jsx
+++ src/ui/pages/Start/components/DrawSidebar.jsx
@@ -55,7 +55,7 @@ export default function DrawSidebar({
                 </div>
                 <div className="selector m-0 flex-shrink-0">
                     <select
-                        className="form-select form-select-sm fs-5"
+                        className="form-select form-select-sm fs-6"
                         aria-label="Zoom"
                         value={zoomIndex}
                         onChange={(e) => changeZoom(e.target.value)}
